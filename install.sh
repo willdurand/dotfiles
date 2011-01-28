5a9ff736635aab19c36fe1d041aba4ce4951cb61
William DURAND <william.durand1@gmail.com>
2011-01-28 19:33:38 +0100

    Improved installer to download and add ornicar's zsh-theme.

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ ln -s $CURRENT/.oh-my-zsh ~/.oh-my-zsh
 ln -s $CURRENT/.scripts ~/.scripts
 ln -s $CURRENT/.zshrc ~/.zshrc
 
+wget --no-check-certificate https://github.com/ornicar/oh-my-zsh/raw/master/themes/ornicar.zsh-theme -O ~/.oh-my-zsh/themes/ornicar.zsh-theme
+
 echo "Installed !"
