88549efbaf2054fc6a213282461ea0711670e411
William DURAND <william.durand1@gmail.com>
2011-05-03 22:08:34 +0200

    Fixed installer

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 #/bin/sh
 
 CURRENT=`pwd`
+
+git submodule update --init
 git submodule foreach git pull origin master
 
 if [ -d ~/.vim ] ; then
-    rm ~/.vim
-    rm ~/.vimrc
+    rm -f ~/.vim
+    rm -f ~/.vimrc
+    rm -f ~/.vim/doc/syntastic.txt
+    rm -f ~/.vim/plugin/syntastic.vim
+    rm -rf ~/.vim/syntax_checkers
 fi
 
 ln -s $CURRENT/.vim ~/.vim
 ln -s $CURRENT/.vimrc ~/.vimrc
 
-ln -s $CURRENT/syntastic/doc ~/.vim/doc
-ln -s $CURRENT/syntastic/plugin ~/.vim/plugin
-ln -s $CURRENT/syntastic/syntax_checkers ~/.vim/syntax_checkers
+cp -R $CURRENT/syntastic/doc/* ~/.vim/doc
+cp -R $CURRENT/syntastic/plugin/* ~/.vim/plugin
+cp -R $CURRENT/syntastic/syntax_checkers ~/.vim/syntax_checkers
 
 echo "Installed !"
