1efaae0758cc18e8bfe603f0c09b1c78e450a43b
William DURAND <william.durand1@gmail.com>
2011-05-04 09:38:41 +0200

    Improved installer

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 CURRENT=`pwd`
 
+# Git
 git submodule update --init
 git submodule foreach git pull origin master
 
+# Remove old installations
 if [ -d ~/.vim ] ; then
     rm -f ~/.vim
     rm -f ~/.vimrc
@@ -13,11 +15,20 @@ if [ -d ~/.vim ] ; then
     rm -rf ~/.vim/syntax_checkers
 fi
 
+# Vim
 ln -s $CURRENT/.vim ~/.vim
 ln -s $CURRENT/.vimrc ~/.vimrc
 
+# Syntastic
 cp -R $CURRENT/syntastic/doc/* ~/.vim/doc
 cp -R $CURRENT/syntastic/plugin/* ~/.vim/plugin
 cp -R $CURRENT/syntastic/syntax_checkers ~/.vim/syntax_checkers
 
+# Command-T
+cd ~/.vim/ruby/command-t
+ruby extconf.rb
+make clean && make
+cd $CURRENT
+
+# Here we go :)
 echo "Installed !"
