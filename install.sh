23327511070c4efe9e042611b4242a09a8cb191e
William DURAND <william.durand1@gmail.com>
2011-07-23 23:57:19 +0200

    Improved installer script

--- install.sh
+++ install.sh
@@ -2,10 +2,34 @@
 
 CURRENT=`pwd`
 
-git submodule init && git submodule update
+echo "Get submodules"
+git submodule update --init
 
+if test -L ~/.oh-my-zsh
+then
+    echo "Removed old oh-my-zsh"
+    rm ~/.oh-my-zsh
+fi
+
+echo "Symlink oh-my-zsh"
 ln -s $CURRENT/.oh-my-zsh ~/.oh-my-zsh
-ln -s $CURRENT/.scripts ~/.scripts
-ln -s $CURRENT/.zshrc ~/.zshrc
+
+if test -L ~/.scripts
+then
+    echo "Removed old scripts"
+    rm ~/.scripts
+fi
+
+echo "Symlink scripts"
+ln -s $CURRENT/scripts ~/.scripts
+
+if test -L ~/.zshrc
+then
+    echo "Removed old .zshrc"
+    rm ~/.zshrc
+fi
+
+echo "Symlink ZSH config"
+ln -s $CURRENT/zshrc ~/.zshrc
 
 echo "Installed !"
