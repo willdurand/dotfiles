26fc86dcb129d14b217c2d2d10729a8fa1ad5126
William DURAND <william.durand1@gmail.com>
2012-12-23 18:00:04 +0100

    Add custom prompt setup

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ This is my ZSH config, install it by running:
 
     ./install.sh
 
-William
+
+Credits
+-------
+
+The Git integration is provided by:
+[https://github.com/jcorbin/zsh-git](https://github.com/jcorbin/zsh-git).
--- install.sh
+++ install.sh
@@ -9,6 +9,16 @@ fi
 
 ln -s $CURRENT/scripts ~/.scripts
 
+if [ ! -d ~/.zsh ] ; then
+    mkdir ~/.zsh
+fi
+
+if [ -L ~/.zsh/func ] ; then
+    rm ~/.zsh/func
+fi
+
+ln -s $CURRENT/func ~/.zsh/func
+
 # ZSH
 if [ -L ~/.zshrc ] ; then
     rm ~/.zshrc
